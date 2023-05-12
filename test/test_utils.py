c966ae8911a2258cd7c8f26dd5d710ff94bfeebd
ed system on behalf of Alan Robertson <ed>
2023-05-13 09:31:48 +1000

    Scaffold update by instructor

---
+++ test/test_utils.py
@@ -0,0 +1,50 @@
+'''
+Dechat Testing Utils
+'''
+from subprocess import Popen, PIPE
+from functools import partial
+
+# This should be the relative path from whever this script is launched to DECHAT
+DECHAT_DIRECTORY = '.'
+SERVER_START_CMD = 'python server.py {hostname} {port}'
+CLIENT_START_CMD = 'python client.py'
+
+# Replacing POPEN so that we don't need to worry about fds and cwd
+Popen = partial(Popen, cwd=DECHAT_DIRECTORY, stdin=PIPE, stdout=PIPE)
+
+def start_server(hostname='localhost', port=8889):
+    '''
+        Starts a server
+        Returns the python process object for of the server process
+        You may find that if you try to re-use a port between servers
+        in short succession that the old port is still bound.
+        As a result this lets you shift which port your server is on
+        between tests.
+    '''
+    server = Popen(SERVER_START_CMD.format(hostname=hostname, port=port))
+    return server
+
+def start_client():
+    '''
+        Starts a client
+        Returns the python process object for of the server process
+
+    '''
+    client = Popen(CLIENT_START_CMD)
+    return client
+
+def client_write(client, msg : str):
+    '''
+        client_write
+        Sends stdin to that client
+    '''
+    client.stdin.write(msg.encode('ascii'))
+
+def client_read(client) -> list:
+    '''
+        client_read
+        Reads stdout from that client
+    '''
+    lines = client.stdout.readlines()
+    lines_str = list(map(lambda x: x.decode('utf-8'), lines))
+    return lines_str
